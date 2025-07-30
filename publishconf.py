0db7452af404f4b23318e22518b464500c87708c
Chris Tapper <mail@cjtapper.com>
2025-07-30 16:23:25 +1000

    Fix syntax in publish.conf

--- publishconf.py
+++ publishconf.py
@@ -8,20 +8,20 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.cjtapper.com'
+SITEURL = "https://www.cjtapper.com"
 RELATIVE_URLS = False
 
 # Uncomment the following to enable feeds
-#FEED_ALL_ATOM = 'feeds/all.atom.xml'
-#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
 
 JINJA_GLOBALS.update(
-    "DEV": False,
+    {"DEV": False},
 )
